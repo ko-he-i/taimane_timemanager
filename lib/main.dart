f9a1696ecb2d4e81c264dbbb1ccabcddf900743b
ko-he-i <ogiharakohei@gmail.com>
2023-11-25 23:08:30 +0900

    修正

--- lib/main.dart
+++ lib/main.dart
@@ -18,7 +18,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return const MaterialApp(
         debugShowCheckedModeBanner: false,
-        title: 'タイマネ/Time Manager',
+        title: 'タイマネ/TimeManager',
         home: LoginPage());
   }
 }
